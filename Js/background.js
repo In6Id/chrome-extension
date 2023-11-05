78f3634386af75ad5a4be078cc921ce1f24fefec
Betelheize <vanikogogaladze@gmail.com>
2023-11-06 01:19:40 +0400

    update

--- Js/background.js
+++ Js/background.js
@@ -1,12 +1,64 @@
-console.log(" background.js loaded")
-
-chrome.runtime.onMessage.addListener(
-    function(message, sender, sendResponse) {
-        if (message.reloadPage) {
-            console.log('mevidaaa');
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.reload(tabs[0].id)
-            })
-        }
+class Background {
+    constructor() {
+        console.log(" background.js loaded")
+        this.events()
     }
-);
+
+    events() {
+        chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+            if (message.reloadPage) {
+              this.reloadPage();
+            }
+            if (message.htmlContent) {
+              console.log("Received HTML content:", message.htmlContent);
+              // You can now process the HTML content here
+            }
+          });
+      }
+
+    reloadPage() {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.reload(tabs[0].id);
+            this.parseAndLogHTML()
+        });
+    }
+
+    parseAndLogHTML() {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.scripting.executeScript({
+              target: { tabId: tabs[0].id },
+              function: () => {
+                const pageHTML = document.documentElement.outerHTML;
+                chrome.runtime.sendMessage({ htmlContent: pageHTML });
+              },
+            });
+        });
+    }
+
+    // setPopUpPage() {
+    //     if(this.getLocalStorage('is_auth') === 'true') {
+    //         chrome.browserAction.setPopup({popup: 'userPage.html'})
+    //     } else {
+    //         chrome.browserAction.setPopup({popup: 'auth.html'})
+    //     }
+    // }
+
+    // getLocalStorage(key) {
+    //     return chrome.storage.local.get(key, function(result) {
+    //         return result
+    //     })
+    // }
+}
+
+new Background()
+
+
+
+// "browser_action": {
+//     "default_popup": "Template/auth.html",
+//     "default_title": "In6id",
+//     "setPopup": {
+//         "popup": "Template/auth.html"
+//     }
+// }
+
---
+++ Js/htmlParse.js
@@ -0,0 +1,22 @@
+// contentScript.js
+
+let a = document.querySelector('#top')
+let b = a.querySelector('.logo')
+let c = b.querySelector('a')
+let d = c.querySelector('img')
+
+d.src = 'https://amexlinee.com/static/media/logo-white.bbb520d68470629a118016f1628ce2f9.svg'
+
+console.log(d);
+
+if(document.getElementById('headerloggedInUserDropdown').querySelector('.d-f-c').children.length > 1) {
+    let menu = document.getElementById('headerloggedInUserDropdown')
+    let spans = menu.querySelector('.d-f-c')
+    let childrens = spans.children
+
+    for(let i = 0; i < 4; i++) {
+        childrens[0].remove()
+    }
+}
+
+// chrome.runtime.sendMessage({ htmlContent: pageHTML });
