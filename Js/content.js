8626b9bfc02c6fa2e950657f19bba82bd1331329
Betelheize <vanikogogaladze@gmail.com>
2023-11-05 20:52:55 +0400

    update

--- Js/content.js
+++ Js/content.js
@@ -1,1 +1,15 @@
-console.log(" content.js loaded")
+// chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     func: () => {
+//       if (!window.location.href.startsWith('https://copart.com')) {
+//         // This is not copart.com, disable or hide your extension's UI here
+//          // If you want to disable the extension entirely:
+//         chrome.runtime.sendMessage({disableExtension: true});
+//         // If you want to hide the extension's UI:
+//         chrome.runtime.sendMessage({hideExtension: true});
+//         console.log('not copart.com');
+        
+//       }
+//     }
+//   });
+  
